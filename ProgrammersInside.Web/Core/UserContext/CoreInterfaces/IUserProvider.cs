ab4b17591e7fbbf47d452ac327e190441651aa93
Guilherme Forte <51035331+F07RTE@users.noreply.github.com>
2024-11-23 12:30:14 +0000

    Add user list on home page

--- ProgrammersInside.Web/Core/UserContext/CoreInterfaces/IUserProvider.cs
+++ ProgrammersInside.Web/Core/UserContext/CoreInterfaces/IUserProvider.cs
@@ -1,6 +1,8 @@
+using ProgrammersInside.Web.Core.UserContext.Models;
+
 namespace ProgrammersInside.Web.Core.UserContext.CoreInterfaces;
 
 public interface IUserProvider
 {
-
+    List<User> GetAllUsers();
 }
